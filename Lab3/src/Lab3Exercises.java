d113d9c491e1378f6b4fd855b63236bb00d2c4e8
4010a4 <4010a4@anon.ece.uprm.edu>
2017-02-08 08:17:03 -0400

    Centered the Java App

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,14 +1,13 @@
 import javax.swing.JFrame;
-
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Efrain Oliveras");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
+		//Adjust .setLocation to aprox the center of the display
+		myFrame.setLocation(800,500);
 		myFrame.setSize(400, 100);
 		myFrame.setVisible(true);
 		
-		//Moving frame to center of screen
-		//Translate Ch.3 
+		
 	}
 }
